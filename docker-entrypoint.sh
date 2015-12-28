6f8e3362dbaa67eee66dc73bf5a1a9a07deccf47
Langs <langslaughed@gmail.com>
2015-12-28 18:25:56 +0800

    fixes script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -49,7 +49,7 @@ if [ ! -f /etc/ocserv/server-key.pem ] || [ ! -f /etc/ocserv/server-cert.pem ]; 
 	encryption_key
 	tls_www_server
 	EOSRV
-	certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
+	certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
 
 	# Create a test user
 	if [ -z "$NO_TEST_USER" ] && [ ! -f /etc/ocserv/ocpasswd ]; then

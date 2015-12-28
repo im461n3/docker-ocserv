b855e7a837720a8dd466fc219b6390ec90fe313b
Langs <langslaughed@gmail.com>
2015-12-28 19:26:07 +0800

    change script

--- cert-client.sh
+++ cert-client.sh
@@ -5,5 +5,5 @@
 certtool --generate-privkey --outfile $1-key.pem
 sed -i "1ccn = "${1}"" client.tmpl
 sed -i "3cemail = ${2}" client.tmpl
-certtool --generate-certificate --load-privkey $1-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template client.tmpl --outfile $1-cert.pem
-openssl pkcs12 -export -inkey $1-key.pem -in $1-cert.pem -name "$1 VPN Client Cert" -certfile ca-cert.pem -out $1.cert.p12
+certtool --generate-certificate --load-privkey $1-key.pem --load-ca-certificate ca.pem --load-ca-privkey ca-key.pem --template client.tmpl --outfile $1-cert.pem
+openssl pkcs12 -export -inkey $1-key.pem -in $1-cert.pem -name "$1 VPN Client Cert" -certfile ca.pem -out $1.cert.p12
